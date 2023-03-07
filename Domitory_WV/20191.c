6d62420d754d9b1a4272ec8e4829c8a4e220cabe
DogJHDOG <100814191+DogJHDOG@users.noreply.github.com>
2023-03-07 23:56:51 +0900

    골드2_20191_줄임말_sol

---
+++ Domitory_WV/20191.c
@@ -0,0 +1,33 @@
+//
+// Created by junhy on 2023-03-07.
+//
+
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+
+char Arr[1000001] = {0,};
+int checker[27] = {0,};
+
+
+int main(){
+
+
+    scanf("%s",Arr);
+
+
+    char l ;
+    do{
+        l = getchar();
+        l = l - 'a';
+        Arr[l] +=1;
+    }while(l==NULL);
+
+    while(1){
+
+
+
+    }
+
+}*/
