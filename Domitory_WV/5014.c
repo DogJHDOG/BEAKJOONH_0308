f476f3a9669de9d4c143245ef9543a301207a240
DogJHDOG <100814191+DogJHDOG@users.noreply.github.com>
2023-03-15 16:19:53 +0900

    실버1_5014_스타트링크_fail

--- Domitory_WV/5014.c
+++ Domitory_WV/5014.c
@@ -1,136 +1,80 @@
 //
 // Created by junhy on 2023-03-14.
 //
-
+/*
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int check[1000000] ={0,};
+int check[1000001] ={0,};
 int F,S,G,U,D;
 int dx[2] ={0,};
+int max= 1000001;
+//F가 전체 층수 1층 to F층까지
+//S가 현재 위치
+//G가 가야하는 위치
+//U가 위로 이동 수
+//D가 아래로 이동수
+
+//아래로 이동해야 할때 S>G일때
+
+//1.아래로 이동이 가능하다면 아래로 이동한다
+//2.아래도 이동이 불가능하다면 depth를 한단계 나추머 위로 이동한다
+//1번과 2번이 진행되기 전에 해당 위치가 이전에 방문하였던 위치인지 확인한다
+//방문하였던 위치가 아닐때만 for문을 진행한다
+//만약 같은 G와 같은 위치에 도달하였다면 멈춘다
+
 int DFS(int x,int depth){
 
-    if(x==G){
-        return depth;
-    }
-    else if(x<G){
-        if(x+dx[0]>=1&&x+dx[0]<=1000000){
-            if(check[x+dx[0]]==0){
-                DFS(x+dx[0],depth+1);
-            }
-            else{
-                return 0;
-            }
-        }
-        else if(x+dx[1]>=1&&x+dx[1]<=1000000) {
-            if(check[x+dx[1]]==0){
-                DFS(x+dx[1],depth+1);
-            }
-            else{
-                return 0;
-            }
-        }
-        else return 0;
 
+    if(x==G) {
+        if (max>depth){
+            max = depth;
+        }
     }
-    else if(x>G){
-        if(x+dx[1]>=1&&x+dx[1]<=1000000){
-            if(check[x+dx[1]]==0){
-                DFS(x+dx[1],depth+1);
-            }
-            else{
-                return 0;
+
+    else {
+        for (int i = 0; i < 2; i++) {
+
+            if (x + dx[i] >= G - U && x + dx[i] >= 1 && x + dx[i] <= F && x + dx[i] <= S + D && check[x + dx[i]] == 0) {
+                check[x + dx[i]] = 1;
+                DFS(x + dx[i], depth + 1);
+
+            } else if (x + dx[i] >= G - U && x + dx[i] >= 1 && x + dx[i] <= S + D && x + dx[i] <= F && check[x + dx[i]] == 1) {
+                break;
             }
         }
-        else if(x+dx[0]>=1&&x+dx[0]<=1000000) {
-            if(check[x+dx[0]]==0){
-                DFS(x+dx[0],depth+1);
-            }
-            else{
-                return 0;
-            }        }
-        else return 0;
     }
+
 }
-
-
-
-
-
-
-int DFS2(int x,int depth){
-
-
-    if(x==G){
-        return depth;
-    }
-        else if(x>G){
-            if(x+dx[0]>=1&&x+dx[0]<=1000000){
-                if(check[x+dx[0]]==0){
-                    DFS(x+dx[0],depth+1);
-                }
-                else{
-                    return 0;
-                }
-            }
-            else if(x+dx[1]>=1&&x+dx[1]<=1000000) {
-                DFS(x+dx[1],depth+1);
-
-            }
-            else return 0;
-        }
-        else if(x<G){
-            if(x+dx[1]>=1&&x+dx[1]<=1000000){
-                if(check[x+dx[1]]==0){
-                    DFS(x+dx[1],depth+1);
-                }
-                else{
-                    return 0;
-                }
-            }
-            else if(x+dx[0]>=1&&x+dx[0]<=1000000) {
-                DFS(x + dx[0], depth + 1);
-            }
-            else return 0;
-    }
-}
-
-
-
-
 
 int main(){
 
 
     scanf("%d %d %d %d %d",&F,&S,&G,&U,&D);
 
-    int dir = G-S;
-    int dfs;
-    if(dir>0){
-        dx[0] = U;
-        dx[1] = -D;
-        dir = dir/ U;
-        dfs = DFS(S + dir * U,dir);
+    int k;
 
-    }
-    else if(dir<0){
-        dx[0] = U;
-        dx[1] = -D;
-        dir = dir / D;
-
-        dfs = DFS2(S-dir*D,dir);
-
+    if(S>G){
+        dx[0] = -D;
+        dx[1] = U;
+        int temp = S - G;
+        if(D!=0)
+        temp = temp / D;
+        k = DFS(S - temp*D,temp);
     }
     else{
-        printf("0");
-        return 0;
+        dx[0] = -U;
+        dx[1] = D;
+        int rng = G;
+        G = S;
+        S = rng;
+        int temp = S - G;
+        if(U!=0)
+        temp = temp / U;
+        k = DFS(S - (temp*U),temp);
     }
 
-    if(dfs!=0){
-        printf("%d",dfs);
-    }
-    else{
-        printf("use the stairs");
-    }
+    if(max == 1000001) printf("use the stairs");
+    else printf("%d",max);
 
-}
+}*/
