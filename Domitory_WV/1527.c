dff30fbd6e35e8e1316f110a6e7aabeb90cf345c
DogJHDOG <100814191+DogJHDOG@users.noreply.github.com>
2023-03-20 16:32:49 +0900

    브론즈1_2748_피보나치2_sol

---
+++ Domitory_WV/1527.c
@@ -0,0 +1,42 @@
+//
+// Created by junhy on 2023-03-18.
+//
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int A, B;
+
+long long result;
+
+void goldMinsoo(long long num)
+
+{
+
+    if (num > B)
+
+        return;
+
+
+
+    if (A <= num && num <= B)
+
+        result++;
+
+
+
+    goldMinsoo(num * 10 + 4);
+
+    goldMinsoo(num * 10 + 7);
+
+}
+
+int main(){
+
+    scanf("%d %d",&A,&B);
+    goldMinsoo(4);
+    goldMinsoo(7);
+
+    printf("%d",result);
+
+}*/
