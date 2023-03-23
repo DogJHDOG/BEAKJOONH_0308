0b723a1f063e52484c3c95d81710b3469c87a681
DogJHDOG <100814191+DogJHDOG@users.noreply.github.com>
2023-03-23 12:41:30 +0900

    실버1_11403_경로찾기_sol

--- Domitory_WV/11403.c
+++ Domitory_WV/11403.c
@@ -7,27 +7,29 @@
 #include <stdio.h>
 
 int Arr[105][105] ={0,};
+int q[100001] = {0,};
 int check[105][105] = {0,};
-int q[10001] = {0,};
-int head = 0;
-int tail = 0;
 int N;
+int head = 0;
+//q[1]애 하나
+//q[2]에 하나
+//이후 해당 좌표에 1이 있다면 전부 1
+//방향 존재
 
-int BFS(){
 
-    while(head < tail){
-        for(int i=1; i<=N; i++){
-            if(Arr[q[head]][i]==1&&check[q[head]][i]==0){
-                Arr[i][i] = 1;
-                q[tail++] = i;
-                check[q[head]][i] = 1;
+int DFS(int k){
 
-            }
+    for(int i =1;i<=N; i++  ){
+        if(Arr[k][i]==1&&check[q[head]][i]==0){
+            Arr[q[head]][i] = 1;
+            check[q[head]][i] = 1;
+            DFS(i);
         }
-        head ++;
     }
 
 }
+
+
 
 int main() {
 
@@ -35,18 +37,14 @@ int main() {
 
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
-            int k =0;
-            scanf("%d", &k);
-            if(k==1){
-                Arr[i][j] = 1;
-                Arr[j][i] = 1;
-            }
+            scanf("%d", &Arr[i][j]);
         }
     }
 
-    q[head] = 1;
-    tail ++;
-    BFS();
+    for(int i =1; i<=N; i++) {
+        q[head] = i;
+        DFS(i);
+    }
 
     for (int i = 1; i <= N; i++) {
         for (int j = 1; j <= N; j++) {
@@ -55,4 +53,4 @@ int main() {
         printf("\n");
     }
 
-}*/
+}*
