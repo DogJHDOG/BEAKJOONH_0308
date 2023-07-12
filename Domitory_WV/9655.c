729a8211021154e17a72a45abd21976f39d7ce98
DogJHDOG <100814191+DogJHDOG@users.noreply.github.com>
2023-07-12 18:29:33 +0900

    실버5_9655_돌게임_sol

---
+++ Domitory_WV/9655.c
@@ -0,0 +1,19 @@
+////
+//// Created by junhy on 2023-07-12.
+////
+//
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main(){
+//
+//    int N = 0;
+//    scanf("%d",&N);
+//
+//    if(N%2==0){
+//        printf("CY");
+//    }else{
+//        printf("SK");
+//    }
+//
+//}
